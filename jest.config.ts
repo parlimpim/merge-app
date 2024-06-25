3f598565c999000b767e376a523a8a51d3d688bc
parlimpim <kolo24045@gmail.com>
2024-06-26 01:22:48 +0700

    setup jest config to not compile scss

--- jest.config.ts
+++ jest.config.ts
@@ -4,7 +4,16 @@ module.exports = {
   coverageDirectory: "coverage",
   coverageReporters: ["json", "html"],
   testEnvironment: "jsdom",
+  testMatch: [
+    "**/__tests__/**/*.+(ts|tsx|js)",
+    "**/?(*.)+(spec|test).+(ts|tsx|js)",
+  ],
   transform: {
     "^.+\\.(t|j)sx?$": "ts-jest",
+  },
+  moduleNameMapper: {
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
+      "<rootDir>/__mocks__/fileMock.js",
+    "\\.(css|less|scss|sass)$": "identity-obj-proxy",
   },
 };
